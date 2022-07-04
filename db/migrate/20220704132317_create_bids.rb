55e0d0975dbde53f56468edd945e60d7799c8075
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-07-04 06:23:57 -0700

    User,Bid,Product Set up

---
+++ db/migrate/20220704132317_create_bids.rb
@@ -0,0 +1,11 @@
+class CreateBids < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bids do |t|
+      t.string :price
+      t.integer :product_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
