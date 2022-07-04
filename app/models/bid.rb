948c191bf9aa739121934a4c2fa032a8d60eca0c
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-07-04 07:31:10 -0700

    seed

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,4 +1,4 @@
 class Bid < ApplicationRecord
-    belongs_to: products
-    belongs_to: users
+    belongs_to :product
+    belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do 
+    Product.create({
+      name: Faker::Book.title,
+      category: "book",
+      price: "$#{rand(1..100000000)}"
+    })
+
+    Product.create({
+      name: Faker::Game.title,
+      category: "game",
+      price: "$#{rand(1..100000000)}"
+    })
+
+    Product.create({
+        name: Faker::JapaneseMedia::OnePiece.character,
+        category: 'figure',
+        price: "$#{rand(1..100000000)}"
+    })
+
+    Product.create({
+        name: Faker::Movie.title,
+        category: 'movie',
+        price: "$#{rand(1..100000000)}"
+    })
+
+    Product.create({
+        name: Faker::Drone.name,
+        category: 'electronic',
+        price: "$#{rand(1..100000000)}"
+    })
+
+    Product.create({
+        name: Faker::Camera.brand_with_model,
+        category: 'electronic',
+        price: "$#{rand(1..100000000)}"
+    })
+
+    Product.create({
+        name: Faker::Coffee.blend_name,
+        category: 'coffee',
+        price: "$#{rand(1..100000000)}"
+    })
+end
+
+
+
+  
