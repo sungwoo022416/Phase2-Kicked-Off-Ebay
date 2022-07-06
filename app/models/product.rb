1d207cba81c16653f04e5afab362ae3b11620d13
Mark Viti <+>
2022-07-06 00:29:27 -0400

    show page looking fire

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,8 @@ class Product < ApplicationRecord
     has_many :product_categories
     has_many :categories, through: :product_categories
     belongs_to :user
+
+    def max_bid
+        self.bids.max { |b1, b2| b1.price <=>b2.price }
+    end
 end
