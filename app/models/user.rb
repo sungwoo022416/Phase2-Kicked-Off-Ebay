6e14d7c8ccecef3f1376981133352750cb058e54
Mark Viti <+>
2022-07-08 09:42:00 -0400

    show user page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,23 @@ class User < ApplicationRecord
     has_many :products, through: :bids
     has_many :interests
     has_many :categories, through: :interests
+
+    def category_1
+        if self.categories != []
+            return "#{self.categories[0].name}"
+        end
+    end
+
+    def category_2
+        if self.categories.count >= 2
+            return " | #{self.categories[1].name}"
+
+        end
+    end
+
+    def category_3
+        if self.categories.count >= 3 
+            return " | #{self.categories[2].name}"
+        end
+    end
 end
